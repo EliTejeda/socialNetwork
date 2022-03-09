d22fcfb7a51de2c0b122ca3e524ed368743817db
Nizcat <nizalli@gmail.com>
2022-03-09 13:32:43 -0600

    implementando SPA

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@
 
 import { myFunction } from './lib/index.js';
 
+import { routes } from './routes';
+
 myFunction();
+const rootDiv = document.getElementById('root');
+
+window.onload = function start() {
+  rootDiv.innerHTML = `<ul>
+<li><a href=""></a>
+</li>
+<li><a href=""></a>
+</li>
+</ul>
+`
+}
