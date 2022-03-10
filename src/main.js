f23196bbc9ca6bea664f3a05d394cada400607d2
Nizcat <nizalli@gmail.com>
2022-03-09 23:08:13 -0600

    prueba SPA constructor

--- src/main.js
+++ src/main.js
@@ -1,30 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-import { routes } from './routes.js';
 
 myFunction();
-let rootDiv = document.getElementById('root');
-
-window.onload = function start() {
-  const buttons = document.createElement('div');
-  buttons.innerHTML = `<ul>
-<li><a href="#" onclick="onNavigate('/about'); return false;" id="logInButton" >LogIn</a>
-</li>
-<li><a href="#">Register</a>
-</li>
-</ul>`
-  rootDiv.appendChild(buttons);
-};
-
-/* const main = '../src/main.js'; */
-
-
-export const onNavigate = (pathname) => {
-  window.history.pushState(
-    {},
-    pathname,
-    window.location.origin + pathname
-  );
-  rootDiv.innerHTML = routes[pathname];
-};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,14 @@
-export const routes = {
-  '/': main,
-  '/logIn': logIn,
-  '/about': about
+const PATHS = {
+  home: {
+    path: '/',
+    template: `<h1> Home</h1>`,
+  },
+  about: {
+    path: '/about',
+    template: `<h1> Sobre mi</h1>`,
+  },
+  contact: {
+    path: '/contact',
+    template: `<h1> Contacto</h1>`,
+  },
 };
