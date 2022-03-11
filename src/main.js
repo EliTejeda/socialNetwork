a86d54c7e564fb2afe7f196d3718886c740c7e3f
Nizcat <nizalli@gmail.com>
2022-03-11 11:55:36 -0600

    antes de createelemet

--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ import { myFunction } from './lib/index.js';
 import { onNavigate } from './app.js';
 
 myFunction();
+
+document.body.onload = function () {
+  onNavigate('/start');
+  document.getElementById('toLogin').addEventListener('click', onNavigate('/login'));
+  document.getElementById('toAccount').addEventListener('click', onNavigate('/account'));
+};
--- src/start.js
+++ src/start.js
@@ -1,7 +1,8 @@
 const start =
-`
-  <ul class="navbar-list">
-  <li class="navbar-item"><a href="#" id="toLogin">Log In</a></li>
-  <li class="navbar-item"><a href="toAccount" onclick="onNavigate('/account'); return false;">Register</a></li>
-</ul>
+
+ `
+<button id="toLogin">Log In</button>
+<button id="toAccount">Create Account</button>
+  
 `;
+console.log(start);
