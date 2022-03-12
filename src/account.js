291bfe86dcef05d2505dee2cf5750911b5d58f43
lylyzyta <lylyzyta@gmail.com>
2022-03-11 18:48:16 -0600

    spa Funcionando

--- src/account.js
+++ src/account.js
@@ -1,3 +1,10 @@
-let account = `
-  <h1>I am contact Page</h1>
-`
+function account() {
+  const rootDiv = document.getElementById('root'); /*elemento que tiene el id root en html*///eslint-disable-line
+  const startButtons = document.createElement('div');
+  startButtons.innerHTML = `
+  <inp
+  <button onclick="onNavigate('/login'); return false;" class="button">Login</button>
+    <button onclick="onNavigate('/account'); return false;" class="button">Account</button>`;
+  rootDiv.appendChild(startButtons);
+}
+export { account };
--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
-// Este es el punto de entrada de tu aplicacion
+// Este es el punto de entrada de tu aplicacion **parte dinamica**
 
 import { myFunction } from './lib/index.js';
-import { onNavigate } from './app.js';
+import { start } from './start.js';
+import { login } from './login.js';
+import { account } from './account.js';
 
-myFunction();
-
-document.body.onload = function () {
-  onNavigate('/start');
+const routes = { /*objeto con distintas propiedades *///eslint-disable-line
+  '/login': login,
+  '/account': account,
+  '/start': start(),
 };
 
-document.getElementById('toLogin').addEventListener('click', onNavigate('/login'));
-document.getElementById('toAccount').addEventListener('click', onNavigate('/account'));
+const rootDiv = document.getElementById('root');
+rootDiv.innerHTML = routes[window.location.pathname];
+const onNavigate = (pathname) => {
+  window.history.pushState(            /* anexa un registro a nuestro historial de navegacion  */ //eslint-disable-line
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+  rootDiv.innerHTML = routes[pathname];
+};
+window.onNavigate = onNavigate; /* queda en dominio de window*/ //eslint-disable-line
+window.onpopstate = () => {       /*actualiza url localizacion *///eslint-disable-line
+  rootDiv.innerHTML = routes[window.location.pathname];
+};
+
+document.body.onload = () => {  /*carga las funciones a la pagina*///eslint-disable-line
+  onNavigate('/start');
+  start();
+};
