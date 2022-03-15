520124f397282f92c2f429dc3f51950cd4144393
lylyzyta <lylyzyta@gmail.com>
2022-03-15 09:40:29 -0600

    Ajustes diseño

--- src/account.js
+++ src/account.js
@@ -7,9 +7,8 @@ function account() {
   <input type = 'text'  name= 'userEmail' id= 'userEmail' class= 'loginInput' placeholder="Correo electrónico">
   <input type = 'text'  name= 'user' id= 'user' class= 'loginInput' placeholder="Usuario">
   <input type = 'text'  name= 'password' id= 'password' class= 'loginInput' placeholder="contraseña">
-  <button onclick="onNavigate('/start'), start(); return false;" class="loginButton">Registrarse</button></div>`;
+  <button onclick="onNavigate('/start'); return false;" class="loginButton">Registrarse</button></div>`;
 
   return accountButtons.innerHTML;
 }
-
 export { account };
